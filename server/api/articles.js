9025c35c94ff59fda5f960f79892440d45e8e58d
unknown <vacatamayo321@gmail.com>
2020-04-23 12:37:38 -0500

    Add try catch

--- server/api/articles.js
+++ server/api/articles.js
@@ -1,4 +1,5 @@
 import Article from "../models/Article";
+import Category from "../models/Category";
 import { Router } from "express";
 
 const router = Router();
@@ -6,11 +7,31 @@ const router = Router();
 router.get("/getRecent/:limit", async (req, res) => {
     const limit = parseInt(req.params.limit);
 
-    const articles = await Article.find()
-    .sort({ createdAt: "desc" })
-    .limit(limit);
+    try {
+        const articles = await Article.find()
+        .sort({ createdAt: "desc" })
+        .limit(limit);
 
-    res.json(articles);
+        res.json(articles);
+    } catch (error) {
+        console.log(error);
+
+        res.json([]);
+    }
+});
+
+router.get("/getArticleById/:articleId", async (req, res) => {
+    const { articleId } = req.params;
+
+    try {
+        const article = await Article.findById(articleId).populate("categories");
+
+        res.json(article);
+    } catch (error) {
+        console.log(error);
+
+        res.json({});
+    }
 });
 
 export default router;
