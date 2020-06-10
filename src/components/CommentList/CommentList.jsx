e3e6ee742127fe2dc6d76d7d3f57e5ca3bd2ca9c
unknown <vacatamayo321@gmail.com>
2020-06-10 13:10:52 -0500

    Refactoring Comments

---
+++ src/components/CommentList/CommentList.jsx
@@ -0,0 +1,66 @@
+import FullScreenLoader from "../FullScreenLoader/";
+import Comment from "./Comment";
+import AddComment from "./AddComment/";
+import Api from "../../ApiController";
+
+import { useState, useEffect } from "react";
+
+const COMMENTS_LIMIT = 10;
+
+export default ({ articleId }) => {
+    const [comments, setComments] = useState([]);
+
+    // LOAD MORE COMMENTS
+    const [skip, setSkip] = useState(0);
+    const [loadMoreClass, setLoadMoreClass] = useState("comments__load-more--active");
+    const [loading, setLoading] = useState(false);
+
+    // GET COMMENTS
+    useEffect(() => {
+        setLoading(true);
+
+        Api.get(`comments/getComments/${articleId}/${COMMENTS_LIMIT}/${skip}`)
+        .then(newComments => {
+            if(newComments.length < COMMENTS_LIMIT) {
+                setLoadMoreClass("");
+            }
+
+            setComments([...comments, ...newComments]);
+            setLoading(false);
+        });
+    }, [skip]);
+
+    const addComment = newComment => {
+        setComments([newComment, ...comments]);;
+    };
+
+    return (
+        <div className="container-fluid">
+            <FullScreenLoader loading={loading}/>
+
+            <div className="row comments">
+                <div className="col-12">
+                    <h3 className="comments__title">Comments</h3>
+                </div>
+                <div className="col-12 col-lg-8">
+                    <AddComment
+                    articleId={articleId}
+                    setLoading={setLoading}
+                    addComment={addComment} />
+                </div>
+                <div className="col-12 col-lg-8">
+                    {comments.map(comment => {
+                        return <Comment key={comment._id} comment={comment} />;
+                    })}
+                </div>
+                <div className="col-12 col-lg-8 d-flex justify-content-center">
+                    <button
+                    className={`comments__load-more ${loadMoreClass}`}
+                    onClick={() => setSkip(skip + COMMENTS_LIMIT)}>
+                        Load more comments
+                    </button>
+                </div>
+            </div>
+        </div>
+    );
+}
--- src/pages/article/[articleId].jsx
+++ src/pages/article/[articleId].jsx
@@ -1,113 +1,11 @@
 import Layout from "../../components/Layout/";
 import Navbar from "../../components/Navbar/";
 import Footer from "../../components/Footer/";
-import Comment from "../../components/Comment/";
-import FullScreenLoader from "../../components/FullScreenLoader/";
 import Article from "../../components/Article";
+import CommentList from "../../components/CommentList/CommentList";
 import Api from "../../ApiController";
 
-import { useState, useEffect } from "react";
-
 import "../../styles/pages/article.scss";
-
-function Comments({ articleId }) {
-    const commentsLimit = 10;
-
-    const [comments, setComments] = useState([]);
-
-    // ADD COMMENT
-    const [name, setName] = useState("");
-    const [comment, setComment] = useState("");
-
-    // LOAD MORE COMMENTS
-    const [skip, setSkip] = useState(0);
-    const [loadMoreClass, setLoadMoreClass] = useState("comments__load-more--active");
-    const [loading, setLoading] = useState(false);
-
-    // GET COMMENTS
-    useEffect(() => {
-        setLoading(true);
-
-        Api.get(`comments/getComments/${articleId}/${commentsLimit}/${skip}`)
-        .then(newComments => {
-            if(newComments.length < commentsLimit) {
-                setLoadMoreClass("");
-            }
-
-            setComments(comments.concat(newComments));
-            setLoading(false);
-        });
-    }, [skip]);
-
-    const addComment = e => {
-        e.preventDefault();
-        e.target.reset();
-
-        setLoading(true);
-
-        Api.post("comments/addComment/", {
-            articleId,
-            name,
-            comment
-        }).then(data => {
-            if(data.status) {
-                const newComments = [];
-                comments.forEach(comment => newComments.push(comment));
-                newComments.unshift(data.createdComment);
-
-                setComments(newComments);
-            }
-
-            setLoading(false);
-        });
-    };
-
-    return (
-        <div className="container-fluid">
-            <FullScreenLoader loading={loading}/>
-            <div className="row comments">
-                <div className="col-12">
-                    <h3 className="comments__title">Comments</h3>
-                </div>
-                <div className="col-12 col-lg-8">
-                    <form onSubmit={addComment}>
-                        <input
-                        type="text"
-                        className="formulary__input mb-2"
-                        maxLength="30"
-                        placeholder="Enter your Name"
-                        onChange={({ target: { value } }) => setName(value)}
-                        required/>
-
-                        <textarea
-                        placeholder="Enter your Message"
-                        className="formulary__textarea"
-                        onChange={({ target: { value } }) => setComment(value)}
-                        maxLength="500"
-                        rows="6"
-                        required></textarea>
-
-                        <button type="submit" className="submit-button mt-3">
-                            Add Comment
-                        </button>
-                    </form>
-                </div>
-                <div className="col-12 col-lg-8">
-                    {comments.map(comment => {
-                        return <Comment key={comment._id} comment={comment} />;
-                    })}
-                </div>
-                <div className="col-12 col-lg-8 d-flex justify-content-center">
-                    <button
-                    className={`comments__load-more ${loadMoreClass}`}
-                    onClick={() => setSkip(skip + commentsLimit)}>
-                        Load more comments
-                    </button>
-                </div>
-            </div>
-        </div>
-    );
-}
 
 function ArticlePage({ article }) {
     return (
@@ -116,7 +14,7 @@ function ArticlePage({ article }) {
 
             <Article article={article} />
 
-            <Comments articleId={article._id}/>
+            <CommentList articleId={article._id} />
 
             <Footer/>
         </Layout>
