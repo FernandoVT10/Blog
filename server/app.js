f5545a19708b266fac0eafaf296d29a8967289e7
unknown <vacatamayo321@gmail.com>
2020-09-11 14:41:59 -0500

    add static files serving

--- server/app.js
+++ server/app.js
@@ -3,7 +3,8 @@ import routes from "./routes";
 import bodyParser from "body-parser";
 
 const app = express();
-    
+
+app.use(express.static(__dirname + "/../public"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
